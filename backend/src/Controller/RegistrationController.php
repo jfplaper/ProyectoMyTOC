d76b5a9bade907a82b3a87fae76fb01cc1281c8b
jfplaper <jfplaper@gmail.com>
2025-03-16 01:31:11 +0100

    Diseño interfaz web

--- backend/src/Controller/RegistrationController.php
+++ backend/src/Controller/RegistrationController.php
@@ -6,35 +6,80 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request, 
+        SluggerInterface $slugger, 
+        UserPasswordHasherInterface $userPasswordHasher, 
+        EntityManagerInterface $entityManager,
+        #[Autowire('%kernel.project_dir%/public/uploads/images')] string $imagesDirectory
+    ): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('plainPassword')->getData();
+            if ($form->isSubmitted() && $form->isValid()) {
+                /** @var string $plainPassword */
+                $plainPassword = $form->get('plainPassword')->getData();
+                $confirmationPlainPassword = $form->get('confirmationPlainPassword')->getData();
+                // Comprueba que el password introducido por el usuario coincide con el de confirmación y si
+                // no coincide muestra un mensaje al usuario y renderiza la vista del registro
+                if ($plainPassword !== $confirmationPlainPassword) {
+                    $this->addFlash('error', 'No coinciden ambos password introducidos');
+                    return $this->redirectToRoute('app_register');
+                }
+    
+                // Codifica el password plano/puro (tal cual lo escribe el usuario sin hash)
+                $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
-            // encode the plain password
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+                /** @var UploadedFile $imageFile */
+                $imageFile = $form->get('image')->getData();
 
-            $entityManager->persist($user);
-            $entityManager->flush();
+                // Cómo el campo image no es requerido pero en la base de datos no puede ser null nos aseguramos 
+                // de que, si el usuario no sube una imagen, le pondremos una por defecto
+                $newFilename = "avatar_default.png";
 
-            // do anything else you need here, like send an email
+                // Esta condición es necesaria porque el campo 'image' no es requerido, de manera que la 
+                // imagen debe ser procesada sólo cuando una imagen es subida
+                if ($imageFile) {
+                    $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    // Esto es necesario para incluir de manera segura el nombre de la imagen como parte de la URL
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
 
-            return $this->redirectToRoute('app_login');
-        }
+                    // Mueve la imagen a la carpeta donde las imágenes subidas por los usuarios son almacenadas
+                    try {
+                        $imageFile->move($imagesDirectory, $newFilename);
+                    } catch (FileException $e) {
+                        throw new FileException("Error al mover y almacenar la imagen subida " . $e->getMessage());
+                    }
+                }
+
+                // Actualiza la propiedad 'image' de la entidad User almacenando el nombre de la imagen 
+                // subida o el del avatar por defecto si el usuario no sube una (pero no la imagen en sí)
+                $user->setImage($newFilename);
+                // Guardo todos los datos restantes
+                $user->setBanned(false);
+                //$user->setEmail($request->get('email')); // No es necesario porque está mapped a true en RegistrationFormType
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('success', '¡Registro finalizado con éxito!');
+                return $this->redirectToRoute('app_login');
+            }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
--- backend/src/Form/RegistrationFormType.php
+++ backend/src/Form/RegistrationFormType.php
@@ -4,43 +4,94 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'attr' => ['class' => 'form-control', 'autofocus' => true],
+            ])
+            ->add('email', EmailType::class, [
+                'mapped' => true,
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Debes rellenar este campo',
+                    ]),
+                    new Assert\Email([
+                        'message' => 'Debes usar un formato de email válido (ejemplo@servicioDeEmail.com).',
+                    ]),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+                'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Deberías estar de acuerdo con nuestros términos.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                // instead of being set onto the object directly, this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Por favor, introduce un password.',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Tu password debería tener al menos {{ limit }} caracteres',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                ],
+            ])
+            ->add('confirmationPlainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor, introduce de nuevo el password.',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Tu password debería tener al menos {{ limit }} caracteres',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Sube una imagen de perfil (jpeg o png)',
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+                'attr' => ['class' => 'px-1'],
+                // make it optional so you don't have to re-upload the PDF file every time you edit the Product details
+                'required' => false,
+                // unmapped fields can't define their validation using attributes in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5000k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Por favor, sube una imagen válida (formato jpeg o png).',
+                    ])
                 ],
             ])
         ;
