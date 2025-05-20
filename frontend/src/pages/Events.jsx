59bc6519e6d00ca5fc9537997c084d123786bce0
jfplaper <jfplaper@gmail.com>
2025-05-20 13:00:37 +0200

    Agregados más filtros, y simultáneos además, a eventos

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -7,20 +7,32 @@ import FavEventsCard from "../components/FavEventsCard";
 const Events = () => {
     const { events, filteredEvents, setFilteredEvents, eventsLoading, favoriteEvents, 
         addEventToFavorites, removeEventFromFavorites } = useEvents();
-    const [selectedFilter, setSelectedFilter] = useState("");
-    const optionsFilter = ["Gratuito", "No gratuito", "Todos"];
+
+    const [selectedPriceFilter, setSelectedPriceFilter] = useState("");
+    const optionsPriceFilter = ["Gratuito", "No gratuito", "Todos"];
+
+    const [selectedDateFilter, setSelectedDateFilter] = useState("");
+    const optionsDateFilter = ["2025-05", "2025-06", "Todas"];
+
+    const [selectedLocationFilter, setSelectedLocationFilter] = useState("");
+    const optionsLocationFilter = ["Granada", "Madrid", "Málaga", "Valencia", "Sevilla", "Bilbao", "Santiago de Compostela", "Barcelona", "Todas"];
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (selectedFilter === "Todos" || selectedFilter === "") {
-            setFilteredEvents(events);
-        } else if (selectedFilter === "Gratuito") {
-            const filtered = events.filter((eve) => !eve.price);
-            setFilteredEvents(filtered);
-        } else { // No gratuito
-            const filtered = events.filter((eve) => eve.price);
-            setFilteredEvents(filtered);
-        }
+        let result = events;
+
+        if (selectedPriceFilter === "Gratuito")
+            result = result.filter(eve => !eve.price);
+        else if (selectedPriceFilter === "No gratuito")
+            result = result.filter(eve => eve.price);
+
+        if (selectedDateFilter && selectedDateFilter !== "Todas")
+            result = result.filter(eve => eve.date.includes(selectedDateFilter));
+
+        if (selectedLocationFilter && selectedLocationFilter !== "Todas")
+            result = result.filter(eve => eve.location.includes(selectedLocationFilter));
+
+        setFilteredEvents(result);
     };
 
     if (eventsLoading) {
@@ -30,14 +42,24 @@ const Events = () => {
             <div className="flex flex-col justify-center items-center">
                 <h2 className="text-3xl font-light text-[#2ABF7A] mt-12 mb-6">EVENTOS PARA LA COMUNIDAD MYTOC</h2>
 
-                {/* Users can filter by price */}
+                {/* Users can filter by price, date and/or location */}
                 <div className="mt-4 mb-8">
                     <form className="flex justify-between items-center gap-2" onSubmit={handleSubmit}>
                         <h2 className="text-xl text-center text-gray-900 whitespace-nowrap">Filtrar por</h2>
                         <select className="p-2 border-gray-200 border rounded-lg focus:outline-[#2AB7FA] text-gray-700 text-sm" 
-                            value={selectedFilter || ""} onChange={(e) => setSelectedFilter(e.target.value)}>
+                            value={selectedPriceFilter || ""} onChange={(e) => setSelectedPriceFilter(e.target.value)}>
                             <option value="">Precio</option>
-                            {optionsFilter.map((option, index) => (<option key={index} value={option}>{option}</option>))}
+                            {optionsPriceFilter.map((option, index) => (<option key={index} value={option}>{option}</option>))}
+                        </select>
+                        <select className="p-2 border-gray-200 border rounded-lg focus:outline-[#2AB7FA] text-gray-700 text-sm" 
+                            value={selectedDateFilter || ""} onChange={(e) => setSelectedDateFilter(e.target.value)}>
+                            <option value="">Fecha</option>
+                            {optionsDateFilter.map((option, index) => (<option key={index} value={option}>{option}</option>))}
+                        </select>
+                        <select className="p-2 border-gray-200 border rounded-lg focus:outline-[#2AB7FA] text-gray-700 text-sm" 
+                            value={selectedLocationFilter || ""} onChange={(e) => setSelectedLocationFilter(e.target.value)}>
+                            <option value="">Localización</option>
+                            {optionsLocationFilter.map((option, index) => (<option key={index} value={option}>{option}</option>))}
                         </select>
                         <button className="p-2 mx-2 rounded-md bg-[#2AB7FA] text-white hover:bg-blue-700" 
                             type="submit">Aplicar</button>
